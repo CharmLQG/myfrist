8efe4270e653d63e7e431c84f2dc9c8a68035c0e
charming <1430871062@qq.com>
2018-01-19 10:36:15 +0800

    Test01

--- src/main/java/com/charming/FristGit.java
+++ src/main/java/com/charming/FristGit.java
@@ -2,6 +2,6 @@ package com.charming;
 
 public class FristGit {
     public static void main(String[] args){
-        System.out.println("Hi Git");
+        System.out.println("Hi Git 11");
     }
 }
